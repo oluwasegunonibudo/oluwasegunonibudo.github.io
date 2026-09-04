61adf89555182a82584b9860dc8b457bcaec1b9d
oluwasegunonibudo <oluwasegunonibudos@gmail.com>
2026-09-04 12:08:36 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,15 @@
+const translations = {
+  en: {
+    navHome: "Home",
+    navAbout: "About Me",
+    navWork: "Work",
+    navResearch: "Research & Publications",
+    navWorkshops: "Workshops",
+    navInternship: "Internship",
+    navProjects: "Projects & Maps",
+    navSocial: "Social Media",
+
+    homeTitle: "Geospatial Analyst • Applied Geologist • Cartographer • Remote Sensing Specialist",
+    homeP1: "I am a Geospatial Analyst and Applied Geologist with over seven years of experience integrating GIS, remote sensing, and geological science to support environmental management, groundwater exploration, land administration, and sustainable development.",
+    homeP2: "I have contributed to major GIS and land administration projects at OYOGIS, KADGIS, and NASRDA, conducted award‑winning research on groundwater prediction and lineament mapping, and published multiple peer‑reviewed studies on geospatial modelling, DEM‑derived lineament extraction, and environmental analysis.",
+    homeP3: "I also train professionals, students, and government staff in GIS and remote sensing, leading geospatial intelligence seminars and capacity‑building programs across Nigeria and China. This portfolio showcases my research, projects, maps, teaching
